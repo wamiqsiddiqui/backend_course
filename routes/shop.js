2ba7b3dfc03d7ba9617ee08efff1e221426cf19e
wamiqsiddiqui <wamiqsiddiqui30@gmail.com>
2025-01-19 15:16:47 +0500

    mvc

--- routes/shop.js
+++ routes/shop.js
@@ -1,24 +1,8 @@
-const path = require("path");
-
 const express = require("express");
-
-const rootDir = require("../util/path");
-const adminData = require("./admin");
+const productsController = require("../controllers/products");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("shop.js", adminData.products);
-  const products = adminData.products;
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render("shop", {
-    prods: products,
-    docTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
