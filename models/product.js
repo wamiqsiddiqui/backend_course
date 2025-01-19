876bbc18047a402f7fe3a4f132c2be705ef182f2
wamiqsiddiqui <wamiqsiddiqui30@gmail.com>
2025-01-19 22:18:20 +0500

    emhancing the app

--- models/product.js
+++ models/product.js
@@ -1,14 +1,35 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, price, description) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {});
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
   }
 };
